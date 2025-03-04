9291b6882fdcb26f687039101045aa24d8e86699
collins muriuki <muriukicollins48@gmail.com>
2025-03-04 12:40:07 +0300

    added addEventListener function

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button');  // Get the button by its ID
+
 function addingEventListener() {
+  input.addEventListener('click', function() {
+    alert('I was clicked!');  // Show an alert when the button is clicked
+  });
 }
+
+addingEventListener();  // Call the function to bind the event listener
